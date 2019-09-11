eed49f73be4971257e5d07db8836366820b5d4b9
Nick Pozoulakis <nicholaspoz@gmail.com>
2019-09-10 22:39:29 -0700

    [cron] added jackson dependency

--- pleo-antaeus-rest/build.gradle.kts
+++ pleo-antaeus-rest/build.gradle.kts
@@ -10,4 +10,5 @@ dependencies {
 
     implementation("io.javalin:javalin:2.6.0")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.9.8")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.8")
 }
