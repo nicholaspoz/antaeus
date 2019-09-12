05e7fe9a877771e2bcbed06b9f19fbdb583999ba
Nick Pozoulakis <nicholaspoz@gmail.com>
2019-09-11 22:07:40 -0700

    [services] setup stub of async webhook handling

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,9 +1,28 @@
 package io.pleo.antaeus.core.services
 
-import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.models.CronJob
+import io.pleo.antaeus.models.CronJobStatus
+import kotlin.concurrent.thread
 
 class BillingService(
-    private val paymentProvider: PaymentProvider
+    private val cronJobService: CronJobService
 ) {
-   // TODO - Add code e.g. here
+    @Synchronized
+    fun runBillingJob(): CronJob {
+        val name = "CRON_JOB"
+        val job = cronJobService.getOrCreateByName(name)
+        if (job.status == CronJobStatus.CREATED) runAsync(job)
+        return job
+    }
+
+    private fun runAsync(job: CronJob) {
+        cronJobService.updateStatusByName(job.name, CronJobStatus.RUNNING)
+        thread {
+            try {
+                Thread.sleep(5000L)
+            } finally {
+                cronJobService.updateStatusByName(job.name, CronJobStatus.FINISHED)
+            }
+        }
+    }
 }
