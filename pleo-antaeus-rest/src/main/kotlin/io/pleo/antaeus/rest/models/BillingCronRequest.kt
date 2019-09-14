5ad87becdb23d84f4f1bad196297c29a1961fff5
Nick Pozoulakis <nicholaspoz@gmail.com>
2019-09-13 20:24:21 -0700

    [job-impl] updated API error handling

--- pleo-antaeus-rest/src/main/kotlin/io/pleo/antaeus/rest/models/BillingCronRequest.kt
+++ pleo-antaeus-rest/src/main/kotlin/io/pleo/antaeus/rest/models/BillingCronRequest.kt
@@ -1,14 +1,21 @@
 package io.pleo.antaeus.rest.models
 
+import io.pleo.antaeus.core.exceptions.InvalidJobTypeException
 import io.pleo.antaeus.core.jobs.JobType
 import org.joda.time.DateTime
 import org.joda.time.format.ISODateTimeFormat
+import java.lang.IllegalArgumentException
 
 data class BillingCronRequest(val jobType: String, val period: String)
 
 fun BillingCronRequest.deserialize(): Pair<JobType, DateTime> {
     // TODO Javalin probably has a way to deserialize complex types
-    val jt = JobType.valueOf(jobType)
+    val jt: JobType
+    try {
+        jt = JobType.valueOf(jobType)
+    } catch (e: IllegalArgumentException) {
+        throw InvalidJobTypeException("Invalid JobType provided.")
+    }
     val pd = ISODateTimeFormat.dateTimeParser().parseDateTime(period)
     return Pair(jt, pd)
 }
